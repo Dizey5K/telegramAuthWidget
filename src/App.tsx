8f3528a12aa30771acaa10663e4c79d5b350373c
Dizey5K <fedorovo727@gmail.com>
2024-11-17 12:52:00 +0300

    ya lox

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <main>
         <TelegramLoginButton
-          botName="name"
+          botName={name}
           buttonSize="large"
           cornerRadius={10}
           usePic={false}
