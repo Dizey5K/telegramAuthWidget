92d0d04fbe53ce34e28da45aa53b8348c07b4cf1
Dizey5K <fedorovo727@gmail.com>
2024-11-17 12:46:32 +0300

    widget works on

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import TelegramLoginButton from "./components/TelegramButton";
+
+function App() {
+  let name = "Tele1234dfdfBot";
+  const handleBot = (user: any) => {
+    console.log(user);
+  };
+
+  return (
+    <>
+      <main>
+        <TelegramLoginButton
+          botName="name"
+          buttonSize="large"
+          cornerRadius={10}
+          usePic={false}
+          dataOnauth={handleBot}
+        />
+      </main>
+    </>
+  );
+}
+
+export default App;
